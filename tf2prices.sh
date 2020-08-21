caa38d6f5b8ed521e5961bdbf17dbcaee2dcadac
Nathan Daly <fleskywood@gmail.com>
2020-08-20 19:36:39 -0500

    truly proper handling of IGetCurrencies.json

--- tf2prices.sh
+++ tf2prices.sh
@@ -7,9 +7,9 @@ apikey=''
 curl -sX GET "https://backpack.tf/api/IGetCurrencies/v1?raw=0&key=$apikey" -H  "accept: application/json" > $HOME/IGetCurrencies.json
 
 #get prices
-ref=$(cat ./IGetCurrencies.json |jq '.response.currencies.metal.price.value')
-key=$(cat ./IGetCurrencies.json |jq '.response.currencies.keys.price.value')
-bud=$(cat ./IGetCurrencies.json |jq '.response.currencies.earbuds.price.value')
+ref=$(cat $HOME/IGetCurrencies.json |jq '.response.currencies.metal.price.value')
+key=$(cat $HOME/IGetCurrencies.json |jq '.response.currencies.keys.price.value')
+bud=$(cat $HOME/IGetCurrencies.json |jq '.response.currencies.earbuds.price.value')
 
 #print prices and nice text
 echo "Ref: $ref Keys: $key Buds: $bud"
